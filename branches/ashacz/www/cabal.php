1d45ec2f1cf1c10dae05c50494fbfa6c53bf1fd6
ashacz <ashacz@aee11931-1e13-0410-913b-b946d4ac90c4>
2006-05-07 14:07:01 +0000

    Probando los permisos de escritura de svn, no tomar en cuenta

--- branches/ashacz/www/cabal.php
+++ branches/ashacz/www/cabal.php
@@ -1,4 +1,5 @@
 <?
+//Prueba 
 // The source code packaged with this file is Free Software, Copyright (C) 2005 by
 // Ricardo Galli <gallir at uib dot es>.
 // It's licensed under the AFFERO GENERAL PUBLIC LICENSE unless stated otherwise.
