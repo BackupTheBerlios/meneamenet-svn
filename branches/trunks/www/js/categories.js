7050c558e7eb497a59cbf861f9127e5a3b45ab3d
trunks <trunks@aee11931-1e13-0410-913b-b946d4ac90c4>
2006-05-27 01:27:28 +0000

    adding aggregator support

--- branches/trunks/www/js/categories.js
+++ branches/trunks/www/js/categories.js
@@ -5,7 +5,15 @@ function edit_category(obj) {
 	obj.id).innerHTML + '"/><label for="new_parent-' + obj.id + '">Hija de:</label>' + 
 	'<input type="text" id="new_parent-' + obj.id + '" name="new_parent-' + 
 	obj.id + '" tabindex="1" size="1" value="' + document.getElementById('parent-' + 
-	obj.id).value +'"/>';
+	obj.id).value +'"/><label for="new_feed-' + obj.id + '">url feed:</label>' + 
+	'<input type="text" id="new_feed-' + obj.id + '" name="new_feed-' + 
+	obj.id + '" tabindex="1" size="25" value="' + document.getElementById('feed-' + 
+	obj.id).value +'"/><br /><!--<span class="genericformnote"><strong>pocas ' + 
+	'palabras, genéricas, cortas y separadas por "," (coma)</strong> ' + 
+	'Ejemplo: <em>web, programación, softwarelibre</em></span>--><br />' + 
+	'<label for="new_tags-' + obj.id + '">etiquetas por defecto:</label><input ' + 
+	'type="text" id="new_tags-' + obj.id + '" name="new_tags-' + obj.id +
+	'" tabindex="1" size="25" /></p>';
 
 	// Para ocultar submit principal y sacar uno único por cada campo.
 	// Habilitar esta opción anularia la posibilidad de actualización
