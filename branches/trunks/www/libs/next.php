2b5dd2a9218889e35673e9d205f36a925c6f6c43
trunks <trunks@aee11931-1e13-0410-913b-b946d4ac90c4>
2006-06-03 13:09:21 +0000

    modifying copyright

--- branches/trunks/www/libs/next.php
+++ branches/trunks/www/libs/next.php
@@ -1,6 +1,6 @@
 <?
 // The source code packaged with this file is Free Software, Copyright (C) 2005 by
-// Ricardo Galli <gallir at uib dot es>.
+// Javier Carranza <javier at al dot quimia dot net>.
 // It's licensed under the AFFERO GENERAL PUBLIC LICENSE unless stated otherwise.
 // You can get copies of the licenses here:
 // 		http://www.affero.org/oagpl.html
